f9b430869998ca532c09b98384c5987603987db7
Antonio Laguna <antoniolaguna97@gmail.com>
2018-08-21 18:14:53 -0500

    Add files via upload

---
+++ Forma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Forma
+    {
+        protected int Base;
+        protected int Altura;
+
+        public void SetBase(int b)
+        {
+            Base = b;
+        }
+        public void SetAltura(int a)
+        {
+            Altura = a;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Herencia
+    {
+        static void Main(string[] args)
+        {
+            int Base = 0;
+            int Altura = 0;
+
+            Console.WriteLine("introduce la base");
+            Base = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Introduce la altura ");
+            Altura = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine(Altura * Base);
+
+            Rectangulo Rect = new Rectangulo();
+            Rect.SetBase(Base);
+            Rect.SetAltura(Altura);
+
+            Console.WriteLine("El área es = {0}", Rect.GetArea());
+
+
+            Console.ReadKey();
+
+
+        }
+    
+    }
+}
